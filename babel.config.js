c7fa15a5b38907fdc321428c92428c689ff41ada
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-08-19 19:00:10 +0200

    chore(babel): configure path aliases

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,26 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@components': './components',
+            '@screens': './screens',
+            '@assets': './assets',
+            '@utils': './utils',
+            '@hooks': './hooks',
+            '@navigation': './navigation',
+            '@store': './store',
+            '@services': './services',
+            '@types': './types',
+            '@': './', // general alias for src
+          },
+          extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        },
+      ],
+    ],
   };
 };
