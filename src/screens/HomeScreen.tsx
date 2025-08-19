b8374b71f04ea3a0e47fbaff39bc8f4e63918ebe
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-08-19 20:29:59 +0200

    feat(screens): add home screen

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>MindSteps 👣</Text>
+    </View>
+  );
+};
+export default HomeScreen;
