a856624495bfb0fbe850d5f60d3eebc90718e22a
Frippanhono <167538940+Frippanhono@users.noreply.github.com>
2025-08-21 13:16:39 +0200

    feat(app): integrera startwalkbutton i app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StartWalkButton from './components/StartWalkButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <StartWalkButton />
       <StatusBar style="auto" />
     </View>
   );
