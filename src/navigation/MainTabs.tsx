36d0ec8bd43bf1cc9cd0fd51621f01904952d98e
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-08-21 15:08:00 +0200

    feat(navigation): add icons to navbar

--- src/navigation/MainTabs.tsx
+++ src/navigation/MainTabs.tsx
@@ -4,15 +4,38 @@ import HomeScreen from '@/screens/HomeScreen';
 import HistoryScreen from '@/screens/HistoryScreen';
 import SettingsScreen from '@/screens/SettingsScreen';
 import NoteScreen from '@/screens/NoteScreen';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
 const Tab = createBottomTabNavigator();
 
 const MainTabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-      <Tab.Screen name="History" component={HistoryScreen} />
-      <Tab.Screen name="Notes" component={NoteScreen} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Hem') {
+            iconName = 'home';
+          } else if (route.name === 'Historik') {
+            iconName = 'history';
+          } else if (route.name === 'Notiser') {
+            iconName = 'bell';
+          } else if (route.name === 'Inställningar') {
+            iconName = 'cog';
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'grey',
+        tabBarInactiveTintColor: 'black',
+        tabBarLabelPosition: 'below-icon',
+      })}
+    >
+      <Tab.Screen name="Hem" component={HomeScreen} />
+      <Tab.Screen name="Historik" component={HistoryScreen} />
+      <Tab.Screen name="Notiser" component={NoteScreen} />
+      <Tab.Screen name="Inställningar" component={SettingsScreen} />
     </Tab.Navigator>
   );
 };
