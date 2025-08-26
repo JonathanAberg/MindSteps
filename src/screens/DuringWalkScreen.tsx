cdc108b6aaf587313963dcbeb50a2961b0463db6
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-08-26 13:25:34 +0200

    fix(type) change type to string

--- src/screens/DuringWalkScreen.tsx
+++ src/screens/DuringWalkScreen.tsx
@@ -15,13 +15,13 @@ const DuringWalkScreen = () => {
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>During Walk</Text>
 
-      <Text> Här ska det ligga en component <StopWatch/>  som tickar under pågående promenad</Text>
+      <Text> Här ska det ligga en component StopWatch  som tickar under pågående promenad</Text>
 
-      <Text> Här ska det ligga en component <StopWalk/> för att avsluta promenaden, skicka info och navigera till LogWalkScreen. Koppla till backend genom hook useEndWalk(som skickar {JSON.stringify(walkInfo)})</Text>
+      <Text> Här ska det ligga en component StopWalk för att avsluta promenaden, skicka info och navigera till LogWalkScreen. Koppla till backend genom hook useEndWalk(som skickar {JSON.stringify(walkInfo)})</Text>
 
-      <Text> Här ska det ligga en component <QuestionDisplay/> för att visa frågorna under promenaden. Frågorna hämtas från frågebatteri valt av användaren i inställnignarna. hook useQuestions</Text>
+      <Text> Här ska det ligga en component QuestionDisplay för att visa frågorna under promenaden. Frågorna hämtas från frågebatteri valt av användaren i inställnignarna. hook useQuestions</Text>
 
-      <Text> Här ska det ligga en component <RecordYourAnswer/> för att spela in svar på frågorna. hook useRecordAnswer</Text>
+      <Text> Här ska det ligga en component RecordYourAnswer för att spela in svar på frågorna. hook useRecordAnswer</Text>
     </SafeAreaView>
   );
 };
