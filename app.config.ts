d2ba4f72b49792d28842b019bf87e1c884fd5b13
Karolinasgithub <karolina.s.lundberg@gmail.com>
2025-08-27 15:36:40 +0200

    chore(app.config.ts): add app.config.ts to use in api-client

---
+++ app.config.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "MindSteps",
+    slug: "mindsteps",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    plugins: ["expo-font"],
+
+    
+    extra: {
+      API_URL: process.env.API_URL,
+    },
+  },
+};
